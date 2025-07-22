3f574725f28ecf853bf8f978778dd9a414f8e8f7
400cs <78524100+400cs@users.noreply.github.com>
2025-07-21 23:46:29 -0700

    setting up frontend

---
+++ frontend/Traffic-Tracker/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.BenNguyen.traffictracker',
+  appName: 'Traffic Tracker',
+  webDir: 'www'
+};
+
+export default config;
