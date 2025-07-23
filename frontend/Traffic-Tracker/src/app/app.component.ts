44b93c013d54ef69ad9cc085c6db294c8ae65e5b
400cs <78524100+400cs@users.noreply.github.com>
2025-07-23 01:02:27 -0700

    created a basic input bar for destination inputs

--- frontend/Traffic-Tracker/src/app/app.component.ts
+++ frontend/Traffic-Tracker/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { IonInput, IonItem, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Traffic-Tracker';
+  title = 'Traffic Tracker';
 }
